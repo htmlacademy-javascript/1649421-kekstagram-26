4acde5e29d83d4c3125180680d378d37c4e19dfb
Ilya-0208-pixe <77454591+Ilya-0208-pixe@users.noreply.github.com>
2022-06-09 22:55:52 +0400

    добавляет функции

--- js/main.js
+++ js/main.js
@@ -1,25 +1,19 @@
-let calculateMiles = function (distance) {
-  let percent = 0.25;
-  if (distance > 10500) {
-    percent = 0.35;
+//имя_функции(проверяемая_строка, максимальная_длина);
+// Результат: true, если строка проходит по длине, и false — если не проходит
+
+let = stringToTest;
+let = maximumLength;
+let calculateMiles = function (stringToTest, maximumLength) {
+  if (maximumLength <= 140) {
+    calculateMiles = true;
+  } else {
+    calculateMiles = false;
   }
-  let miles = Math.floor(distance * percent);
-  return miles;
-};
+}
 
-console.log('За перелёт в Иркутск получим ' + calculateMiles(4125) + ' миль');
-console.log('За перелёт на Камчатку получим ' + calculateMiles(11000) + ' миль');
+//имя_функции(от, до); // Результат: целое число из диапазона "от...до"
 
-//https://up.htmlacademy.ru/profession/frontender/13/javascript/26/module/2/item/14/8
-
-let calculateSum = function (numberFirst, numberSecond) {
-  let sum = numberFirst + numberSecond;
-  return sum;
-};
-
-calculateSum(); // Вернёт NaN
-calculateSum(2); // Вернёт NaN
-calculateSum(2, 5); // Вернёт 7
-calculateSum(9, 5); // Вернёт 14
-
-//https://up.htmlacademy.ru/profession/frontender/13/javascript/26/module/2/item/14/9
+let nameFunctions = function (fromNumber, upNumber) {
+  let result = numberFirst < numberSecond;
+  return result;
+}
