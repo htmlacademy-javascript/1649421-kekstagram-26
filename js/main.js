fc9077cab93cdb031094d1ae8712020c9c6d6152
Ilya-0208-pixe <77454591+Ilya-0208-pixe@users.noreply.github.com>
2022-07-21 21:44:05 +0400

    8,2

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ import { createObjectsList } from "./data.js";
 
 import { renderThumbnails } from "./thumbnails.js";
 
+import { upLoadForm } from "./form.js";
+
 const NUMBER_OBJECTS = 25;
 const objectList = createObjectsList(NUMBER_OBJECTS);
 renderThumbnails(objectList);
+upLoadForm();
