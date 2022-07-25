8df4714dd5c646752c43e07baca582cbd3ffe603
Ilya-0208-pixe <77454591+Ilya-0208-pixe@users.noreply.github.com>
2022-07-25 21:20:04 +0400

    1101

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-import { createObjectsList } from "./data.js";
+import { createObjectsList } from './data.js';
 
-import { renderThumbnails } from "./thumbnails.js";
+import { renderThumbnails } from './thumbnails.js';
 
-import { upLoadForm } from "./form.js";
+import { upLoadForm } from './form.js';
+
+import { createLoader } from './fetch.js';
 
 const NUMBER_OBJECTS = 25;
 const objectList = createObjectsList(NUMBER_OBJECTS);
 renderThumbnails(objectList);
 upLoadForm();
+createLoader();
+console.log(createLoader());
